17d36a0f3608dbb1222d0d103542448d119bf91e
Fabián Toledo <fabian.toledo@alumnos.ucn.cl>
2024-04-16 01:34:31 -0400

    Revert "sin metodos correo para que no de errores al probar"
    This reverts commit b0b35ed9449674514c4017fcaf106e5b7caf0e09.

--- src/http/auth/auth.service.ts
+++ src/http/auth/auth.service.ts
@@ -13,6 +13,7 @@ import { compare } from 'bcrypt';
 import { hash } from 'bcrypt';
 import { randomBytes } from 'crypto';
 import { differenceInMinutes } from 'date-fns';
+import { EmailService } from './email.service';
 import * as nodemailer from 'nodemailer';
 
 @Injectable()
@@ -23,6 +24,7 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly jwtService: JwtService,
+    private readonly emailService: EmailService,
   ) {}
 
   async createPasswordResetToken(
@@ -51,6 +53,54 @@ export class AuthService {
       await this.userRepository.save(user);
     } catch (error) {
       throwHttpException(HttpStatus.INTERNAL_SERVER_ERROR, 'Error saving user');
+      return;
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.mailgun.org',
+      port: 587,
+      auth: {
+        user: 'postmaster@sandboxe9d7672a884f4c6bbd740cd8dab7e513.mailgun.org',
+        pass: '291e6b1289c99859dab46ccb22bdd38a-19806d14-f5a67df8',
+      },
+    });
+
+    const mailOptions = {
+      to: user.email,
+      from: 'postmaster@sandboxe9d7672a884f4c6bbd740cd8dab7e513.mailgun.org',
+      subject: 'Node.js Password Reset',
+      text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
+      Please click on the following link, or paste this into your browser to complete the process:\n\n
+      http://localhost:3000/reset/${token}\n\n
+      If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+    };
+
+    try {
+      if (!mailOptions.to) {
+        throw new Error('Recipient email is not defined');
+      }
+      await this.emailService.sendText(
+        mailOptions.to,
+        mailOptions.subject,
+        mailOptions.text,
+      );
+    } catch (error) {
+      if (error.code === 'ECONNECTION') {
+        throwHttpException(
+          HttpStatus.INTERNAL_SERVER_ERROR,
+          'Error de conexión al servidor SMTP',
+        );
+      } else if (error.code === 'EAUTH') {
+        throwHttpException(
+          HttpStatus.INTERNAL_SERVER_ERROR,
+          'Error de autenticación SMTP',
+        );
+      } else {
+        throwHttpException(
+          HttpStatus.INTERNAL_SERVER_ERROR,
+          'Error enviando el correo electrónico',
+        );
+      }
       return;
     }
   }
---
+++ src/http/auth/email.service.ts
@@ -0,0 +1,42 @@
+import * as nodemailer from 'nodemailer';
+import * as nodemailerMailgunTransport from 'nodemailer-mailgun-transport';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const mailgunOptions = {
+  auth: {
+    api_key: process.env.MAILGUN_ACTIVE_API_KEY || '',
+    domain: process.env.MAILGUN_DOMAIN || '',
+  },
+};
+
+const transport = nodemailerMailgunTransport(mailgunOptions);
+
+export class EmailService {
+  private emailClient: nodemailer.Transporter;
+
+  constructor() {
+    this.emailClient = nodemailer.createTransport(transport);
+  }
+
+  sendText(to: string, subject: string, text: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.emailClient.sendMail(
+        {
+          from: '"Your Name" <youremail@yourdomain.com>',
+          to,
+          subject,
+          text,
+        },
+        (err, info) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(info);
+          }
+        },
+      );
+    });
+  }
+}
