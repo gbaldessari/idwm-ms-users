6a9dca19be8bb04e884aea08a52806cb693722ab
EduMirandaC <eduardo.miranda01@alumnos.ucn.cl>
2024-05-17 14:30:29 -0400

    .env.example corregido

--- src/http/auth/auth.service.ts
+++ src/http/auth/auth.service.ts
@@ -121,7 +121,7 @@ export class AuthService {
       const payload = {id: user?.id  ,email: user?.email};
       const access_token = await this.jwtService.signAsync(payload);
 
-      return  access_token;
+      return {token: access_token};
 
   }
 
